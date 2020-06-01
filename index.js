399c3129113a033808017c961c65f620c2de6513
Alireza Rezania <alireza.rzna@gmail.com>
2020-06-01 12:12:47 +0430

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+const express = require("express");
+
+const app = express();
+
+app.get("/url/:url", (req, res) => {
+  const url = req.params.url;
+  const token = req.query.t;
+  fetch(url, { headers: { Authorization: token } })
+    .then((r) => r.blob())
+    .then((result) => res.sendFile(result))
+    .catch((err) => res.send("Error loading image"));
+});
+
+app.listen(80, () => {
+  console.log("Server listening");
+});
