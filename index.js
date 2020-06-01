c29d23f95c978e599e19e6f4348fb58965b06b70
Alireza Rezania <alireza.rzna@gmail.com>
2020-06-01 12:28:26 +0430

    fix send blob

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const app = express();
 app.get("/url", (req, res) => {
   const url = req.query.url;
   const token = req.query.t;
+  console.log(url, token);
   fetch(url, { headers: { Authorization: token } })
     .then((r) => r.blob())
-    .then((result) => res.sendFile(result))
-    .catch((err) => res.send("Error loading image", err));
+    .then((blob) => {
+      res.type(blob.type);
+      blob.arrayBuffer().then((buf) => {
+        res.send(Buffer.from(buf));
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(200).send("error loading image");
+    });
 });
 
-app.listen(process.env.PORT || 80, () => {
+app.listen(process.env.PORT || 3200, () => {
   console.log("Server listening");
 });
